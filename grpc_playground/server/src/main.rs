718bb709e7335121c68cbe8c0ea0a1fa3fe5b461
Norman Link <norman.link@gmx.net>
2020-12-17 14:50:09 +0100

    [impl] added Server.Dockerfile

--- grpc_playground/server/src/main.rs
+++ grpc_playground/server/src/main.rs
@@ -1,6 +1,7 @@
 mod hello;
 mod services;
 
+use structopt::StructOpt;
 use tonic::transport::Server;
 use futures::prelude::*;
 
@@ -10,9 +11,18 @@ use services::ServerStreamingService;
 use services::ClientStreamingService;
 use services::BidirectionalStreamingService;
 
+#[derive(StructOpt)]
+#[structopt(about = "A gRPC test server")]
+struct Cli {
+    #[structopt(short, help = "The port on which the gRPC server will be opened")]
+    port: u32,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse().unwrap();
+    let args = Cli::from_args();
+
+    let addr = format!("127.0.0.1:{}", args.port).parse().unwrap();
 
     println!("Server listening on {}", addr);
 
